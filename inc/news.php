a7ab06ad1c2f8cf5dc8c6357fb8a68a249cf0c48
bognar marton <bognarmarci@gmail.com>
2012-09-19 17:28:53 +0200

    news.php fix

--- inc/news.php
+++ inc/news.php
@@ -1,41 +1,46 @@
 <?php
 
-if (isset($_GET["id"])) {
+if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
 // DISPLAY ONE PIECE OF NEWS
 
 	$query = mysql_query("SELECT * FROM news WHERE id=".$_GET["id"]);
-	$row = mysql_fetch_assoc($query);
 	
-	echo "<h1>".$row["title"]."</h1>";
+	if (mysql_num_rows($query) == 1) {
+	
+		$row = mysql_fetch_assoc($query);
+	
+		echo "<h1>".$row["title"]."</h1>";
 		
-		echo "<p>".nl2br($row["text"])."</p>";
-		echo $row["date"]." - ".$row["author"];
+			echo "<p>".nl2br($row["text"])."</p>";
+			echo $row["date"]." - ".$row["author"];
 		
-		echo "Comments";
-		if ($row["comments"] == 1) {
-			$cq = mysql_query("SELECT * FROM newscomments WHERE newsid=".$row["id"]." ORDER BY id ASC");
-			$commnum = mysql_num_rows($cq);
-			echo "<a href='?p=news&id=".$row["id"]."#comments'>".$commnum." comments</a>";
+			echo "Comments";
+			if ($row["comments"] == 1) {
+				$cq = mysql_query("SELECT * FROM newscomments WHERE newsid=".$row["id"]." ORDER BY id ASC");
+				$commnum = mysql_num_rows($cq);
+				echo "<a href='?p=news&id=".$row["id"]."#comments'>".$commnum." comments</a>";
 			
-			while ($crow = mysql_fetch_assoc($cq)) {
-				echo "<div class='comment'>";
-				echo $crow["username"]." - ".$crow["date"];
-				echo "<p>".nl2br($crow["text"])."</p>";
-				echo "</div>";
-			}
+				while ($crow = mysql_fetch_assoc($cq)) {
+					echo "<div class='comment'>";
+					echo $crow["username"]." - ".$crow["date"];
+					echo "<p>".nl2br($crow["text"])."</p>";
+					echo "</div>";
+				}
 			
-			if (checkuser()) {			
-			echo "<form>
-			<textarea></textarea>
-			<input type='submit' />
-			</form>";
-			} else {
-			echo "you have to be logged in to post comments";
-			}
+				if (checkuser()) {			
+					echo "<form>
+					<textarea></textarea>
+					<input type='submit' />
+					</form>";
+				} else {
+					echo "you have to be logged in to post comments";
+				}
 			
-		} else 
-			echo "commenting disabled";
-
+			} else 
+				echo "commenting disabled";
+	} else {
+		echo "No.";
+	}
 } else {
 // DISPLAY ALL THE NEWS
 
