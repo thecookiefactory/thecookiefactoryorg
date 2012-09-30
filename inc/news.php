33686a54c0f99ef323e6507d4d37367e94a3218a
Nagy Bence <underer@gmail.com>
2012-10-01 00:37:07 +0200

    comment area css finalized

--- inc/news.php
+++ inc/news.php
@@ -36,12 +36,12 @@ if (isset($_GET["id"]) && is_numeric($_GET["id"])) {
 			
 				$cq = mysql_query("SELECT * FROM newscomments WHERE newsid=".$row["id"]." ORDER BY id ASC");
 				$commnum = mysql_num_rows($cq);
-				echo "<hr><a name='comments'></a>Comments<a href='?p=news&id=".$row["id"]."#comments' id='comments-title'><br>".$commnum." comments</a>";
+				echo "<hr><a name='comments'></a><a href='?p=news&id=".$row["id"]."#comments' id='comments-title'>".$commnum." comments</a><br>";
 			
 				while ($crow = mysql_fetch_assoc($cq)) {
 					echo "<div class='comment'><span class='comment-metadata'>";
-					echo "<span class='comment-author'>".$crow["username"]."</span> - <span class='comment-date'>".$crow["date"]."</span>";
-					echo "</span><br><p><span class='comment-text'>".nl2br($crow["text"])."</span></p>";
+					echo "<span class='comment-author'>".$crow["username"]."</span> &middot; <span class='comment-date'>".$crow["date"]."</span>";
+					echo "</span><br><p class='comment-text'><span class='comment-text'>".nl2br($crow["text"])."</span></p>";
 					echo "</div>";
 				}
 
