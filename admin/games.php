0623997641cdc79ff10531f56b69d5dd7c40ca44
bognar marton <bognarmarci@gmail.com>
2012-12-02 15:07:49 +0100

    games managing and a fix

--- admin/games.php
+++ admin/games.php
@@ -17,9 +17,30 @@ if (!checkadmin()) die("403");
 
 <?php
 
-$query = mysqli_query($con, "SELECT * FROM games");
+$query = mysqli_query($con, "SELECT * FROM `games`");
 $nr = mysqli_num_rows($query);
 
+if (isset($_POST["update"])) {
+
+    while ($r = mysqli_fetch_assoc($query)) {
+	$id = $r["id"];
+	$name = strip($_POST[$id."name"]);
+	$steam = strip($_POST[$id."steam"]);
+	
+	mysqli_query($con, "UPDATE `games` SET `name`='".$name."', `steam`='".$steam."' WHERE `id`=".$id);
+	}
+
+}
+
+if (isset($_POST["addnew"])) {
+
+    $name = strip($_POST["name"]);
+    $steam = strip($_POST["steam"]);
+	mysqli_query($con, "INSERT INTO `games` VALUES('','".$name."','".$steam."')");
+
+}
+
+$query = mysqli_query($con, "SELECT * FROM `games`");
 echo "<h1>manage games</h1>";
 
 echo "<form action='games.php' method='post'>";
@@ -27,15 +48,17 @@ echo "<form action='games.php' method='post'>";
 echo "<table border>";
 echo "<tr><th>id</th><th>name</th><th>steam store id</th></tr>";
 while ($row = mysqli_fetch_assoc($query)) {
-echo "<tr><td>".$row["id"]."</td><td><input type='text' value='".$row["name"]."' name=''></td><td><input type='text' value='".$row["steam"]."' name=''></td></tr>";
+
+	echo "<tr><td>".$row["id"]."</td><td><input type='text' value='".$row["name"]."' name='".$row["id"]."name'></td><td><input type='text' value='".$row["steam"]."' name='".$row["id"]."steam'></td></tr>";
+
 }
 echo "</table>";
 
-echo "<input type='submit' value='update'>";
+echo "<input type='submit' value='update' name='update'>";
 echo "</form>";
 echo "<hr>";
 echo "<form action='games.php' method='post'>
-<input type='text' name=''><input type='text' name=''><input type='submit' value='add new'>
+<input type='text' name='name'><input type='text' name='steam'><input type='submit' name='addnew' value='add new'>
 </form>";
 
 ?>
