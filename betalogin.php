375a2c6186a1e3e4f2f6852c53a6c1800b7e7fcd
bognar marton <bognarmarci@gmail.com>
2012-12-09 16:39:46 +0100

    a bit of refactoring

--- betalogin.php
+++ betalogin.php
@@ -5,12 +5,17 @@ $r_c = 42;
 require "inc/essential.php";
 
 if (isset($_POST["beta"])) {
-$key = strip($_POST["beta"]);
-$q = mysqli_query($con, "SELECT * FROM `betatest` WHERE `key`='".$key."'");
-if (mysqli_num_rows($q) == 1) {
-echo "<a href='index.php?p=news'>welcome</a>";
-$_SESSION["beta"] = $key;
-} else { }
+    
+	$key = strip($_POST["beta"]);
+    $q = mysqli_query($con, "SELECT * FROM `betatest` WHERE `key`='".$key."'");
+
+	if (mysqli_num_rows($q) == 1) {
+
+	    echo "<a href='index.php?p=news'>welcome</a>";
+        $_SESSION["beta"] = $key;
+
+	}
+
 }
 
 ?>
