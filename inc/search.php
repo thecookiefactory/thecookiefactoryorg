f28cc0e7ebd95f18f1d496db9635eb3305baebb7
bognar marton <bognarmarci@gmail.com>
2013-06-08 22:14:30 +0200

    fixed and improved stuff in search

--- inc/search.php
+++ inc/search.php
@@ -15,26 +15,25 @@ if (isset($_POST["searchb"]) && strip($_POST["searchb"]) != "") {
 
     if (strlen($term) >= 3) {
 
-        if (!isset($_POST["inn"]) && !isset($_POST["inf"])) {
-            $_POST["inn"] = "on";
-        }
-
-        if (isset($_POST["inn"]) && $_POST["inn"] == "on") {
+        if (isset($_POST["inn"]) || !isset($_POST["inf"])) {
+            
+            $nsearch = true;
             $squery = mysqli_query($con, "SELECT * FROM `news` WHERE `text` LIKE '%".$term."%' or `title` LIKE '%".$term."%' ORDER BY `id` DESC");
             $nr = mysqli_num_rows($squery);
+            $sss = ($nr == 1) ? "" : "s";
 
             if ($nr == 0) {
 
                 if (strlen($term) > 23) {
                     ?>
 
-                    <div class='search-title'>No <button class="search-type" type="button">result</button>s found for your search term</div>
+                    <div class='search-title'>No <?php echo resultbutton(); ?>s found for your search term</div>
 
                     <?php
                 } else {
                     ?>
 
-                    <div class='search-title'>No <button class="search-type" type="button">result</button>s found for <span class='search-term'><?php echo $term; ?></span></div>
+                    <div class='search-title'>No <?php echo resultbutton(); ?>s found for <span class='search-term'><?php echo $term; ?></span></div>
 
                     <?php
                 }
@@ -42,33 +41,17 @@ if (isset($_POST["searchb"]) && strip($_POST["searchb"]) != "") {
             } else {
 
                 if (strlen($term) > 23) {
-                    if ($nr == 1) {
-                        ?>
+                    ?>
 
-                        <div class='search-title'><?php echo $nr; ?> <button class="search-type" type="button">result</button> found for your search term</div>
+                    <div class='search-title'><?php echo $nr; ?> <?php echo resultbutton(); ?><?php echo $sss; ?> found for your search term</div>
 
-                        <?php
-                    } else {
-                        ?>
-
-                        <div class='search-title'><?php echo $nr; ?> <button class="search-type" type="button">result</button>s found for your search term</div>
-
-                        <?php
-                    }
+                    <?php
                 } else {
-                    if ($nr == 1) {
-                        ?>
+                    ?>
 
-                        <div class='search-title'><?php echo $nr; ?> <button class="search-type" type="button">result</button> found for <span class='search-term'><?php echo $term; ?></span></div>
+                    <div class='search-title'><?php echo $nr; ?> <?php echo resultbutton(); ?><?php echo $sss; ?> found for <span class='search-term'><?php echo $term; ?></span></div>
 
-                        <?php
-                    } else {
-                        ?>
-
-                        <div class='search-title'><?php echo $nr; ?> <button class="search-type" type="button">result</button>s found for <span class='search-term'><?php echo $term; ?></span></div>
-
-                        <?php
-                    }
+                    <?php
                 }
 
                 ?>
@@ -89,7 +72,11 @@ if (isset($_POST["searchb"]) && strip($_POST["searchb"]) != "") {
 
                     if ($srow["comments"] == 1) {
 
-                        $cq = mysqli_query($con, "SELECT `id` FROM `forumposts` WHERE `tid`=".$srow["id"]);
+                        $ct = mysqli_query($con, "SELECT `id` FROM `forums` WHERE `newsid`=".$srow["id"]);
+                        $tid = mysqli_fetch_assoc($ct);
+                        $tid = $tid["id"];
+
+                        $cq = mysqli_query($con, "SELECT `id` FROM `forumposts` WHERE `tid`=".$tid);
                         $commnum = mysqli_num_rows($cq);
                         ?>
 
@@ -134,15 +121,16 @@ if (isset($_POST["searchb"]) && strip($_POST["searchb"]) != "") {
                 </div>
 
                 <?php
-        }
-        }
-
-        if (isset($_POST["inf"]) && $_POST["inf"] == "on") {
+            }
+        
+        } else {
 
             $squery1 = mysqli_query($con, "SELECT * FROM `forums` WHERE `text` LIKE '%".$term."%' or `title` LIKE '%".$term."%' ORDER BY `id` DESC") or die(mysqli_error($con));
             $squery2 = mysqli_query($con, "SELECT * FROM `forumposts` WHERE `text` LIKE '%".$term."%' ORDER BY `id` DESC") or die(mysqli_error($con));
+            $nr = mysqli_num_rows($squery1) + mysqli_num_rows($squery2);
+            $sss = ($nr == 1) ? "" : "s";
             ?>
-            <?php echo mysqli_num_rows($squery1) + mysqli_num_rows($squery2); ?> results found in the forums
+            <?php echo $nr." ".resultbutton().$sss; ?> found in the forums
             <?php
 
             while ($srow = mysqli_fetch_assoc($squery1)) {
@@ -170,4 +158,9 @@ if (isset($_POST["searchb"]) && strip($_POST["searchb"]) != "") {
     <?php
 }
 
-?>
+function resultbutton() {
+    global $term;
+    global $nsearch;
+    $name = (isset($nsearch) && $nsearch == true) ? "inf" : "inn";
+    return "<form method='post' action='?p=search'><input type='hidden' value='".$term."' name='searchb'><input class='search-type' value='result' type='submit' name='".$name."'></form>";
+}
