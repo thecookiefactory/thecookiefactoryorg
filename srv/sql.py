fbffb5275396efc5737c8b015beaedd33fd9d590
Bence Nagy <underer@gmail.com>
2013-07-20 22:28:00 +0200

    Fix Python lint warnings

--- srv/sql.py
+++ srv/sql.py
@@ -3,15 +3,15 @@ from config import *
 
 
 class SQLConnection:
-  def __init__(self):
-    self.open()
+    def __init__(self):
+        self.open()
 
-  def open(self):
-    self.con = mysql.connector.connect(**dbconfig)
-    self.crs = self.con.cursor()
+    def open(self):
+        self.con = mysql.connector.connect(**dbconfig)
+        self.crs = self.con.cursor()
 
-  def close(self, commit=True):
-    if commit:
-      self.con.commit()
-    self.con.close()
-    self.crs.close()
+    def close(self, commit=True):
+        if commit:
+            self.con.commit()
+        self.con.close()
+        self.crs.close()
--- srv/twitch.py
+++ srv/twitch.py
@@ -5,38 +5,38 @@ API_ADDRESS = 'https://api.twitch.tv/kraken/'
 
 
 def getStreamNames(sql):
-  sql.crs.execute('SELECT `twitch` FROM `streams`')
-  return [twitch[0] for twitch in sql.crs]
+    sql.crs.execute('SELECT `twitch` FROM `streams`')
+    return [twitch[0] for twitch in sql.crs]
 
 
 def getFromAPI(method, param=''):
-  return requests.get(API_ADDRESS + '/' + method + '/' + param).json()
+    return requests.get(API_ADDRESS + '/' + method + '/' + param).json()
 
 
 def insertStreamTitle(sql, stream, title):
-  if title:
-    sql.crs.execute("UPDATE `streams` SET `title`='{t}' WHERE `twitch`='{s}'".format(t=title, s=stream))
-  else:
-    sql.crs.execute("UPDATE `streams` SET `title`=NULL WHERE `twitch`='{s}'".format(s=stream))
+    if title:
+        sql.crs.execute("UPDATE `streams` SET `title`='{t}' WHERE `twitch`='{s}'".format(t=title, s=stream))
+    else:
+        sql.crs.execute("UPDATE `streams` SET `title`=NULL WHERE `twitch`='{s}'".format(s=stream))
 
 
 def main():
-  sql = SQLConnection()
-  streamnames = getStreamNames(sql)
-  streamdata = {}
+    sql = SQLConnection()
+    streamnames = getStreamNames(sql)
+    streamdata = {}
 
-  for name in streamnames:
-    streamdata[name] = False
-    streamjson = getFromAPI('streams', name)
+    for name in streamnames:
+        streamdata[name] = False
+        streamjson = getFromAPI('streams', name)
 
-    if streamjson['stream']:
-      streamdata[name] = streamjson['stream']['channel']['status']
+        if streamjson['stream']:
+            streamdata[name] = streamjson['stream']['channel']['status']
 
-  for stream in streamdata:
-    insertStreamTitle(sql, stream, streamdata[stream])
+    for stream in streamdata:
+        insertStreamTitle(sql, stream, streamdata[stream])
 
-  sql.close()
+    sql.close()
 
 
 if __name__ == '__main__':
-  main()
+    main()
