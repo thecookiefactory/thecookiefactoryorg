ee5fb7f715ead896209007efb9b156160f788986
Bence Nagy <underer@gmail.com>
2013-07-29 03:18:24 +0200

    Update github.py to work with new admin panel code

--- srv/github.py
+++ srv/github.py
@@ -5,8 +5,8 @@ API_ADDRESS = 'https://api.github.com/repos'
 
 
 def getRepoNames(sql):
-    sql.crs.execute('SELECT `repo` FROM `maps`')
-    return [repo[0] for repo in sql.crs]
+    sql.crs.execute('SELECT `dl` FROM `maps`')
+    return [dl[0] for dl in sql.crs]
 
 
 def getFromAPI(repo, method, param=''):
@@ -16,27 +16,29 @@ def getFromAPI(repo, method, param=''):
     return requests.get(link).json()
 
 
-def insertAssetLink(sql, repo, link):
+def insertAssetLink(sql, dl, link):
     if link:
-        sql.crs.execute("UPDATE `maps` SET `link`='{t}' WHERE `repo`='{s}'".format(t=link, s=repo))
+        sql.crs.execute("UPDATE `maps` SET `link`='{t}' WHERE `dl`='{s}'".format(t=link, s=repo))
     else:
-        sql.crs.execute("UPDATE `maps` SET `link`=NULL WHERE `repo`='{s}'".format(s=repo))
+        sql.crs.execute("UPDATE `maps` SET `link`=NULL WHERE `dl`='{s}'".format(s=repo))
 
 
 def main():
     sql = SQLConnection()
-    reponames = getRepoNames(sql)
-    repodata = {}
+    dlnames = getDlNames(sql)
+    dldata = {}
 
-    for name in reponames:
-        repodata[name] = False
-        repojson = getFromAPI(name, 'releases')
-        if repojson:
-            assetjson = requests.get(repojson[0]['assets_url']).json()
-            repodata[name] = 'https://github.com/{repo}/releases/download/{releasename}/{assetname}'.format(repo=name, releasename=repojson[0]['name'], assetname=assetjson[0]['name'])
-
-    for repo in repodata:
-        insertAssetLink(sql, repo, repodata[repo])
+    for name in dlnames:
+        dldata[name] = False
+        if '/' in name:
+            repojson = getFromAPI(name, 'releases')
+            if repojson:
+                assetjson = requests.get(repojson[0]['assets_url']).json()
+                dldata[name] = 'https://github.com/{repo}/releases/download/{releasename}/{assetname}'.format(repo=name, releasename=repojson[0]['name'], assetname=assetjson[0]['name'])
+        else:
+            dldata[name] = 'http://steamcommunity.com/sharedfiles/filedetails/?id={workshopid}'.format(workshopid=name)
+    for repo in dldata:
+        insertAssetLink(sql, repo, dldata[repo])
 
     sql.close()
 
