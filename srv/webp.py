a6cde0ecc662282121276f1a29de5920f581c06d
Bence Nagy <underer@gmail.com>
2013-07-31 03:00:07 +0200

    Add exception handling to WebP converter python script

--- srv/webp.py
+++ srv/webp.py
@@ -3,9 +3,16 @@ import sys
 
 
 def main():
-    image = Image.open(sys.argv[1])
-    image.save(sys.argv[1].rsplit('.', 1)[0] + '.webp')
-
+    try:
+        image = Image.open(sys.argv[1])
+    except IndexError:
+        sys.exit('No input file specified.')
+    except OSError:
+        sys.exit('Invalid input file.')
+    try:
+        image.save(sys.argv[1].rsplit('.', 1)[0] + '.webp')
+    except PermissionError:
+        sys.exit('No sufficient permissions to save output file.')
 
 if __name__ == '__main__':
     main()
