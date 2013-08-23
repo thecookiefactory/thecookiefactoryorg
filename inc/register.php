4f4cf65ff112a2774c506e94fe15bd86ad44fd37
Bence Nagy <underer@gmail.com>
2013-08-23 08:47:00 +0200

    Modify header height

--- inc/register.php
+++ inc/register.php
@@ -23,20 +23,20 @@ if (checkuser()) {
             register($username);
 
         }
-        
+
         ?>
 
         <div class='account-form'><form action='?p=register' method='post'>
         <span class='account-text'><span class='account-title'>Hey there!</span><br>My name is </span>
         <input class='account-input' pattern='\w{2,10}' type='text' placeholder='username' name='username' required='required' autocomplete='off' oninput='checkInputBox(this, true);' autofocus>
-        <input class='account-input account-button' type='submit' value='just get me in already!' name='submit'>
+        <input class='account-input account-button' type='submit' value='and I am ready to roll!' name='submit'>
         </form></div>
 
         <?php
 
     } else {
 
-        echo "in order to register you need to login through steam first";
+        echo "In order to register you need to log in through Steam first. Sorry, we tried our best!";
 
     }
 
