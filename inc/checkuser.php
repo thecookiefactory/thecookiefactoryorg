3e30032191af25cc26f480b88b641934f3667ea7
bognar marton <bognarmarci@gmail.com>
2013-08-27 18:06:46 +0200

    added an experimental checkuser.php

---
+++ inc/checkuser.php
@@ -0,0 +1,29 @@
+<?php
+
+$r_c = 1;
+
+require "functions.php";
+
+if (vf($_GET["name"])) {
+
+    $q = $con->prepare("SELECT `users`.`id` FROM `users` WHERE `users`.`name` = :name");
+    $q->bindValue("name", strip($_GET["name"]), PDO::PARAM_STR);
+    $q->execute();
+
+    if ($q->rowCount() === 0) {
+
+        echo "0";
+
+    } else {
+
+        echo "1";
+
+    }
+
+} else {
+
+    header("Location: /notfound.php");
+
+}
+
+?>
