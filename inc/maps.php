9da119a93817aa7efadb2615aecc8dadd51cc49f
Bence Nagy <underer@gmail.com>
2013-10-20 18:15:03 +0200

    Add ad unit to maps page

--- inc/maps.php
+++ inc/maps.php
@@ -125,6 +125,15 @@ if ($q->rowCount() != 0) {
 
             ?>
             <div class='ads'>
+                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- Maps Inline -->
+                  <ins class="adsbygoogle"
+                       style="display:inline-block;width:728px;height:90px"
+                       data-ad-client="ca-pub-8578399795841431"
+                       data-ad-slot="8918199475"></ins>
+                  <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
             </div>
             <?php
 
