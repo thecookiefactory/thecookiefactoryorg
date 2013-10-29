2c4a7b4a6638ef2f622e237ecf5b8e2b14eef077
bognar marton <bognarmarci@gmail.com>
2013-10-29 17:16:22 +0100

    Add some functions to user.class.php

--- inc/classes/user.class.php
+++ inc/classes/user.class.php
@@ -17,15 +17,95 @@ class user {
      * @var $date object
      * @var $twitchname string
      */
-    protected $id;
-    protected $name;
-    protected $steamid;
-    protected $admin;
-    protected $cookieh;
-    protected $date;
-    protected $twitchname;
+    protected $id           = null;
+    protected $name         = null;
+    protected $steamid      = null;
+    protected $admin        = null;
+    protected $cookieh      = null;
+    protected $date         = null;
+    protected $twitchname   = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `users` WHERE `users`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id           = $srow["id"];
+                $this->name         = $srow["name"];
+                $this->steamid      = $srow["steamid"];
+                $this->admin        = $srow["admin"];
+                $this->cookieh      = $srow["cookieh"];
+                $this->date         = new dtime($srow["date"]);
+                $this->twitchname   = $srow["twitchname"];
+
+            } else {
+
+                echo "Could not find a user with the given id.";
+
+            }
+
+        }
+
+    }
+
+    public function getName($span = false) {
+
+        if (!$this->isAdmin() || $span !== true) {
+
+            return $this->name;
+
+        } else if (isset($span) && $span === true) {
+
+            return "<span class='admin-name'>" . $this->name . "</span>";
+
+        }
+
+    }
+
+    public function getTwitchName() {
+
+        return $this->twitchname;
+
+    }
+
+    public function isAdmin() {
+
+        if ($this->admin != 0) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public function isLoggedIn() {
+
+        if ($this->id != null) {
+
+            return true;
+
+        }
+
+        return false;
 
     }
 
