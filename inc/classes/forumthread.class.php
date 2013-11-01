c3bd1acd264e65f72b05ca166e51304ae870e396
bognar marton <bognarmarci@gmail.com>
2013-11-02 00:37:30 +0100

    Add a constructor to forumthread.class.php

--- inc/classes/forumthread.class.php
+++ inc/classes/forumthread.class.php
@@ -21,19 +21,59 @@ class forumthread {
      * @var $news object
      * @var $closed int
      */
-    protected $id;
-    protected $title;
-    protected $text;
-    protected $author;
-    protected $date;
-    protected $editdate;
-    protected $lastdate;
-    protected $forumcategory;
-    protected $map;
-    protected $news;
-    protected $closed;
+    protected $id               = null;
+    protected $title            = null;
+    protected $text             = null;
+    protected $author           = null;
+    protected $date             = null;
+    protected $editdate         = null;
+    protected $lastdate         = null;
+    protected $forumcategory    = null;
+    protected $map              = null;
+    protected $news             = null;
+    protected $closed           = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `forumthreads` WHERE `forumthreads`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id               = $srow["id"];
+                $this->title            = $srow["title"];
+                $this->text             = $srow["text"];
+                $this->author           = $srow["author"];
+                $this->date             = new dtime($srow["date"]);
+                $this->editdate         = new dtime($srow["editdate"]);
+                $this->lastdate         = new dtime($srow["lastdate"]);
+                $this->forumcategory    = new forumcategory($srow["forumcategory"]);
+                $this->map              = ($srow["mapid"] != 0) ? new map($srow["mapid"]) : null;
+                $this->news             = ($srow["newsid"] != 0) ? new news($srow["newsid"]) : null;
+                $this->closed           = $srow["closed"];
+
+            } else {
+
+                echo "Could not find a thread with the given id.";
+
+            }
+
+        }
 
     }
 
