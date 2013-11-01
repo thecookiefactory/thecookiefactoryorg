5c6754cfbc8736c59ec1716f03afc9c21f5f3b54
bognar marton <bognarmarci@gmail.com>
2013-11-02 00:51:37 +0100

    Add a constructor to stream.class.php

--- inc/classes/stream.class.php
+++ inc/classes/stream.class.php
@@ -14,12 +14,45 @@ class stream {
      * @var $text string
      * @var $author object
      */
-    protected $id;
-    protected $title;
-    protected $text;
-    protected $author;
+    protected $id       = null;
+    protected $title    = null;
+    protected $text     = null;
+    protected $author   = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `streams` WHERE `streams`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id       = $srow["id"];
+                $this->title    = $srow["title"];
+                $this->text     = $srow["text"];
+                $this->author   = $srow["author"];
+
+            } else {
+
+                echo "Could not find a stream with the given id.";
+
+            }
+
+        }
 
     }
 
