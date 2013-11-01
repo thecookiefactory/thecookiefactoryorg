8bc2a927718e8f9a2ee923032c196a6eadedccd5
bognar marton <bognarmarci@gmail.com>
2013-11-02 00:55:13 +0100

    Add a constructor to forumpost.class.php

--- inc/classes/forumpost.class.php
+++ inc/classes/forumpost.class.php
@@ -16,14 +16,47 @@ class forumpost {
      * @var $editdate object
      * @var $thread object
      */
-    protected $id;
-    protected $text;
-    protected $author;
-    protected $date;
-    protected $editdate;
-    protected $thread;
+    protected $id       = null;
+    protected $text     = null;
+    protected $author   = null;
+    protected $date     = null;
+    protected $editdate = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `forumposts` WHERE `forumposts`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id       = $srow["id"];
+                $this->text     = $srow["text"];
+                $this->author   = $srow["author"];
+                $this->date     = new dtime($srow["date"]);
+                $this->editdate = new dtime($srow["editdate"]);
+
+            } else {
+
+                echo "Could not find a post with the given id.";
+
+            }
+
+        }
 
     }
 
