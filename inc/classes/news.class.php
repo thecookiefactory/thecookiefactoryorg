e25d43cfd0b9929308a94754ef14b40345004c37
bognar marton <bognarmarci@gmail.com>
2013-11-02 01:23:01 +0100

    Add a constructor to news.class.php

--- inc/classes/news.class.php
+++ inc/classes/news.class.php
@@ -20,18 +20,57 @@ class news {
      * @var $live int
      * @var $stringid string
      */
-    protected $id;
-    protected $title;
-    protected $text;
-    protected $author;
-    protected $date;
-    protected $editor;
-    protected $editdate;
-    protected $comments;
-    protected $live;
-    protected $stringid;
+    protected $id       = null;
+    protected $title    = null;
+    protected $text     = null;
+    protected $author   = null;
+    protected $date     = null;
+    protected $editor   = null;
+    protected $editdate = null;
+    protected $comments = null;
+    protected $live     = null;
+    protected $stringid = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `news` WHERE `news`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id       = $srow["id"];
+                $this->title    = $srow["title"];
+                $this->text     = $srow["text"];
+                $this->author   = new user($srow["authorid"]);
+                $this->date     = new dtime($srow["date"]);
+                $this->editor   = new user($srow["editorid"]);
+                $this->editdate = new dtime($srow["editdate"]);
+                $this->comments = $srow["comments"];
+                $this->live     = $srow["live"];
+                $this->stringid = $srow["stringid"];
+
+            } else {
+
+                echo "Could not find a piece of news with the given id.";
+
+            }
+
+        }
 
     }
 
