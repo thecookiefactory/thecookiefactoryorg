0c022c54caba34c43790147af3ae0dffd3b62982
bognar marton <bognarmarci@gmail.com>
2013-11-02 01:17:55 +0100

    Add a constructor to map.class.php

--- inc/classes/map.class.php
+++ inc/classes/map.class.php
@@ -22,20 +22,61 @@ class map {
      * @var $link string
      * @var $downloadcount int
      */
-    protected $id;
-    protected $name;
-    protected $text;
-    protected $author;
-    protected $date;
-    protected $editdate;
-    protected $dl;
-    protected $extension;
-    protected $comments;
-    protected $game;
-    protected $link;
-    protected $downloadcount;
+    protected $id               = null;
+    protected $name             = null;
+    protected $text             = null;
+    protected $author           = null;
+    protected $date             = null;
+    protected $editdate         = null;
+    protected $dl               = null;
+    protected $extension        = null;
+    protected $comments         = null;
+    protected $game             = null;
+    protected $link             = null;
+    protected $downloadcount    = null;
 
     public function __construct($id = null) {
+
+        global $con;
+
+        if ($id != null) {
+
+            try {
+
+                $squery = $con->prepare("SELECT * FROM `maps` WHERE `maps`.`id` = :id");
+                $squery->bindValue("id", $id, PDO::PARAM_INT);
+                $squery->execute();
+
+            } catch (PDOException $e) {
+
+                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+            }
+
+            if ($squery->rowCount() == 1) {
+
+                $srow = $squery->fetch();
+
+                $this->id               = $srow["id"];
+                $this->name             = $srow["name"];
+                $this->text             = $srow["text"];
+                $this->author           = new user($srow["authorid"]);
+                $this->date             = new dtime($srow["date"]);
+                $this->editdate         = new dtime($srow["editdate"]);
+                $this->dl               = $srow["dl"];
+                $this->extension        = $srow["extension"];
+                $this->comments         = $srow["comments"];
+                $this->game             = new game($srow["gameid"]);
+                $this->link             = $srow["link"];
+                $this->downloadcount    = $srow["downloadcount"];
+
+            } else {
+
+                echo "Could not find a map with the given id.";
+
+            }
+
+        }
 
     }
 
