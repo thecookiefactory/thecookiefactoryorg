3730099768e3a80ecb0aaadd67f8808acc2a6d6b
bognar marton <bognarmarci@gmail.com>
2013-11-12 23:14:55 +0100

    Almost OOify the user's end of news
    Won't work until forums are converted

--- inc/classes/news.class.php
+++ inc/classes/news.class.php
@@ -3,6 +3,7 @@
 if (!isset($r_c)) header("Location: /notfound.php");
 
 require_once str_repeat("../", $r_c) . "inc/classes/dtime.class.php";
+require_once str_repeat("../", $r_c) . "inc/classes/forumthread.class.php";
 require_once str_repeat("../", $r_c) . "inc/classes/user.class.php";
 
 /**
@@ -36,21 +37,39 @@ class news {
     protected $live     = null;
     protected $stringid = null;
 
-    public function __construct($id = null) {
+    public function __construct($id = null, $field = null) {
 
         global $con;
 
         if ($id != null) {
 
-            try {
+            if ($field == "stringid") {
 
-                $squery = $con->prepare("SELECT * FROM `news` WHERE `news`.`id` = :id");
-                $squery->bindValue("id", $id, PDO::PARAM_INT);
-                $squery->execute();
+                try {
 
-            } catch (PDOException $e) {
+                    $squery = $con->prepare("SELECT * FROM `news` WHERE `news`.`stringid` = :id");
+                    $squery->bindValue("id", $id, PDO::PARAM_STR);
+                    $squery->execute();
 
-                echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+                } catch (PDOException $e) {
+
+                    echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+                }
+
+            } else {
+
+                try {
+
+                    $squery = $con->prepare("SELECT * FROM `news` WHERE `news`.`id` = :id");
+                    $squery->bindValue("id", $id, PDO::PARAM_INT);
+                    $squery->execute();
+
+                } catch (PDOException $e) {
+
+                    echo "An error occured while trying to fetch data to the class. (" . $e->getMessage() . ")";
+
+                }
 
             }
 
@@ -69,9 +88,120 @@ class news {
                 $this->live     = $srow["live"];
                 $this->stringid = $srow["stringid"];
 
+            }
+
+        }
+
+    }
+
+    public function display($loc = null) {
+
+        global $con;
+        global $r_c;
+        global $p;
+
+        ?>
+
+        <div class='article-header'>
+        <div class='article-title'>
+        <h1>
+
+        <?php
+
+        if ($loc == "main") {
+
+            echo $this->title;
+
+        } else {
+
+            echo "<a href='/news/" . $this->stringid . "'>" . $this->title . "</a>";
+
+        }
+
+        ?>
+
+        </h1>
+        </div>
+
+        <div class='article-metadata'>
+
+        <?php
+
+        if ($loc == "front" && $this->comments == 1) {
+
+            $ct = $con->prepare("SELECT `forumthreads`.`id` FROM `forumthreads` WHERE `forumthreads`.`newsid` = :id");
+            $ct->bindValue("id", $this->id, PDO::PARAM_INT);
+            $ct->execute();
+
+            $tid = $ct->fetch();
+
+            $thread = new forumthread($tid["id"]);
+            $commnum = $thread->replyCount();
+            ?>
+
+            <?php
+            if ($commnum != 1) {
+                ?>
+                <span class='article-metadata-item'><a href='/news/<?php echo $this->stringid; ?>#comments'><?php echo $commnum; ?> comments</a></span>
+                <?php
+            } else {
+                ?>
+                <span class='article-metadata-item'><a href='/news/<?php echo $this->stringid; ?>#comments'><?php echo $commnum; ?> comment</a></span>
+                <?php
+            }
+
+        }
+
+        ?>
+
+        <span class='article-metadata-item'><span class='article-author'><?php echo $this->author->getName(); ?></span></span>
+        <span class='article-metadata-item'><span class='article-date'><?php echo $this->date->display(); ?></span></span>
+        </div>
+
+        <?php
+
+        if ($this->editor != null && $this->editdate > $this->date) {
+            ?>
+
+            <div class='article-edit-metadata'>
+            <span class='article-metadata-item'><span class='article-author'><?php echo $this->editor->getName(); ?></span></span>
+            <span class='article-metadata-item'><span class='article-date'><?php echo $this->editdate->display(); ?></span></span>
+            </div>
+
+            <?php
+        }
+
+        ?>
+
+        </div>
+        <article>
+        <span class='article-text'><?php echo Markdown($this->text); ?></span>
+        </article>
+        <?php
+
+        if ($loc == "main") {
+
+            echo "<hr id='comments'>";
+
+            if ($this->comments == 1) {
+
+                $sq = $con->prepare("SELECT `forumthreads`.`id` FROM `forumthreads` WHERE `forumthreads`.`newsid` = :id");
+                $sq->bindValue("id", $this->id, PDO::PARAM_INT);
+                $sq->execute();
+
+                $tid = $sq->fetch();
+
+                $tid = $tid["id"];
+
+                require "inc/forums.php";
+
             } else {
 
-                echo "Could not find a piece of news with the given id.";
+                ?>
+
+                <h1 class='comments-title'>Commenting disabled</h1>
+
+                <?php
 
             }
 
@@ -79,6 +209,16 @@ class news {
 
     }
 
-}
+    public function getStringId() {
 
-?>
+        return $this->stringid;
+
+    }
+
+    public function isReal() {
+
+        return ($this->id != null);
+
+    }
+
+}
--- inc/news.php
+++ inc/news.php
@@ -3,6 +3,7 @@
 if (!isset($r_c)) header("Location: /notfound.php");
 
 include_once "analyticstracking.php";
+require_once "inc/classes/news.class.php";
 require_once "markdown/markdown.php";
 
 $_SESSION["lp"] = "news";
@@ -21,7 +22,7 @@ if (!isset($_GET["id"]) || !vf($_GET["id"])) {
     }
 
     $xo = ($page - 1) * 5;
-    $query = $con->prepare("SELECT `news`.`id`, `news`.`title`, `news`.`text`, `news`.`authorid`, `news`.`date`, `news`.`editorid`, `news`.`editdate`, BIN(`news`.`comments`), `news`.`stringid`
+    $query = $con->prepare("SELECT `news`.`id`
                             FROM `news`
                             WHERE `news`.`live` = 1
                             ORDER BY `news`.`id` DESC
@@ -51,72 +52,10 @@ if (!isset($_GET["id"]) || !vf($_GET["id"])) {
 
             $iii++;
 
-            // TITLE, AUTHOR & DATE
+            $news = new news($row["id"]);
+            $news->display("front");
+            
             ?>
-
-            <div class='article-header'>
-            <div class='article-title'><h1><a href='/news/<?php echo $row["stringid"]; ?>'><?php echo $row["title"]; ?></a></h1></div>
-            <div class='article-metadata'>
-
-            <?php
-
-            if ($row["BIN(`news`.`comments`)"] == 1) {
-
-                $ct = $con->prepare("SELECT `forumthreads`.`id` FROM `forumthreads` WHERE `forumthreads`.`newsid` = :id");
-                $ct->bindValue("id", $row["id"], PDO::PARAM_INT);
-                $ct->execute();
-
-                $tid = $ct->fetch();
-
-                $tid = $tid["id"];
-
-                $cq = $con->prepare("SELECT `forumposts`.`id` FROM `forumposts` WHERE `forumposts`.`threadid` = :tid");
-                $cq->bindValue("tid", $tid, PDO::PARAM_INT);
-                $cq->execute();
-
-                $commnum = $cq->rowCount();
-                ?>
-
-                <?php
-                if ($commnum != 1) {
-                    ?>
-                    <span class='article-metadata-item'><a href='/news/<?php echo $row["stringid"]; ?>#comments'><?php echo $commnum; ?> comments</a></span>
-                    <?php
-                } else {
-                    ?>
-                    <span class='article-metadata-item'><a href='/news/<?php echo $row["stringid"]; ?>#comments'><?php echo $commnum; ?> comment</a></span>
-                    <?php
-                }
-
-            }
-
-            ?>
-
-            <span class='article-metadata-item'><span class='article-author'><?php echo getname($row["authorid"]); ?></span></span><span class='article-metadata-item'><span class='article-date'><?php echo displaydate($row["date"]); ?></span></span></div>
-
-            <?php
-
-            //if edited
-            if ($row["editorid"] > 0 && $row["editdate"] > $row["date"]) {
-                ?>
-
-                <div class='article-edit-metadata'><span class='article-metadata-item'><span class='article-author'><?php echo getname($row["editorid"]); ?></span></span><span class='article-metadata-item'><span class='article-date'><?php echo displaydate($row["editdate"]); ?></span></span></div>
-
-                <?php
-            }
-
-            ?>
-
-            </div>
-
-            <?php
-
-            // BODY
-            ?>
-
-            <article>
-            <span class='article-text'><?php echo Markdown($row["text"]); ?></span>
-            </article>
             <hr class='article-separator'>
 
             <?php
@@ -167,64 +106,14 @@ if (!isset($_GET["id"]) || !vf($_GET["id"])) {
 
     // DISPLAY ONE PIECE OF NEWS
 
-    $id = strip($_GET["id"]);
+    $news = new news(strip($_GET["id"]), "stringid");
 
-    $query = $con->prepare("SELECT `news`.`id`, `news`.`title`, `news`.`text`, `news`.`authorid`, `news`.`date`, `news`.`editorid`, `news`.`editdate`, BIN(`news`.`comments`)
-                            FROM `news`
-                            WHERE `news`.`stringid` = :id");
-    $query->bindValue("id", $id, PDO::PARAM_STR);
-    $query->execute();
+    if ($news->isReal()) {
 
-    if ($query->rowCount() == 1) {
-
-        $row = $query->fetch();
-
-        ?>
-
-        <div class='article-header'>
-        <div class='article-title'><h1><?php echo $row["title"]; ?></h1></div><div class='article-metadata'>
-
-        <span class='article-metadata-item'><span class='article-author'><?php echo getname($row["authorid"]); ?></span></span><span class='article-metadata-item'><span class='article-date'><?php echo displaydate($row["date"]); ?></span></span></div>
-
-        <?php
-        //if edited
-        if ($row["editorid"] > 0 && $row["editdate"] > $row["date"]) {
-            ?>
-
-            <div class='article-edit-metadata'><span class='article-metadata-item'><span class='article-author'><?php echo getname($row["editorid"]); ?></span></span><span class='article-metadata-item'><span class='article-date'><?php echo displaydate($row["editdate"]); ?></span></span></div>
-
-            <?php
-        }
-        ?>
-
-        </div><article>
-        <span class='article-text'><?php echo Markdown($row["text"]); ?></span>
-        </article><hr id='comments'>
-
-        <?php
-
-        if ($row["BIN(`news`.`comments`)"] == 1) {
-
-            $ct = $con->prepare("SELECT `forumthreads`.`id` FROM `forumthreads` WHERE `forumthreads`.`newsid` = :id");
-            $ct->bindValue("id", $row["id"], PDO::PARAM_INT);
-            $ct->execute();
-
-            $tid = $ct->fetch();
-
-            $tid = $tid["id"];
-            require_once "forums.php";
-
-            } else {
-                ?>
-
-                <h1 class='comments-title'>Commenting disabled</h1>
-
-                <?php
-            }
+        $news->display("main");
 
     } else {
 
-        // redirecting to the main page instead of giving an error message
         header("Location: /news");
 
     }
