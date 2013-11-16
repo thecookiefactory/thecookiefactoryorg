86acc18e15bba20d2a6feb64c9b94c613b0f5144
bognar marton <bognarmarci@gmail.com>
2013-11-16 02:40:59 +0100

    Fix a missing require

--- inc/classes/forumthread.class.php
+++ inc/classes/forumthread.class.php
@@ -5,6 +5,7 @@ if (!isset($r_c)) header("Location: /notfound.php");
 require_once str_repeat("../", $r_c) . "inc/classes/dtime.class.php";
 require_once str_repeat("../", $r_c) . "inc/classes/forumcategory.class.php";
 require_once str_repeat("../", $r_c) . "inc/classes/forumpost.class.php";
+require_once str_repeat("../", $r_c) . "inc/classes/map.class.php";
 require_once str_repeat("../", $r_c) . "inc/classes/news.class.php";
 require_once str_repeat("../", $r_c) . "inc/classes/user.class.php";
 
