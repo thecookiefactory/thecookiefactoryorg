70e8452030e271d694e9cdf669f4a77f4aeefbe7
bognar marton <bognarmarci@gmail.com>
2013-12-07 01:21:10 +0100

    Templatify news, split up stream template

--- inc/news.php
+++ inc/news.php
@@ -6,6 +6,8 @@ include_once "analyticstracking.php";
 require_once "inc/classes/news.class.php";
 require_once "inc/markdown/markdown.php";
 
+$newsArray = array();
+
 $_SESSION["lp"] = "news";
 
 if (!isset($_GET["id"]) || !vf($_GET["id"])) {
@@ -39,67 +41,42 @@ if (!isset($_GET["id"]) || !vf($_GET["id"])) {
         <?php
     } else {
 
-        $iii = 0;
+        $pageCount = ceil($con->query("SELECT `news`.`id` FROM `news` WHERE `news`.`live` = 1")->rowCount() / 5);
 
         while ($foundNews = $selectNewsByPage->fetch()) {
 
-            $iii++;
-
             $news = new news($foundNews["id"]);
-            $news->display("front");
-
-            if ($iii == 1) {
-                ?>
-                <div class='news-ad'>
-                    <script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
-                    <!-- News Articles Inline -->
-                    <ins class='adsbygoogle'
-                         style='display:inline-block;width:728px;height:90px'
-                         data-ad-client='ca-pub-8578399795841431'
-                         data-ad-slot='5270925477'></ins>
-                    <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-                <hr class='article-separator'>
-                <?php
-
-            }
-
-        }
-    }
-
-    //page links
-    $nr = $con->query("SELECT `news`.`id` FROM `news` WHERE `news`.`live` = 1")->rowCount();
-
-    echo "<div class='news-pages'>";
-
-    for ($i = 1; $i <= ceil($nr / 5); $i++) {
-
-        if ($page == $i) {
-
-            echo "<div class='news-page-number'>" . $i . "</div>";
-
-        } else {
-
-            echo "<a class='news-page-number' href='/news/page/" . $i . "'>" . $i . "</a>";
+            $newsArray[$foundNews["id"]] = $news->returnArray();
 
         }
 
-    }
+        echo $twig->render("news.html", array("news" => $newsArray, "pagecount" => $pageCount, "page" => $page, "main" => 1));
 
-    echo "</div>";
-    echo "<a class='news-rsslink' href='/rss.xml'>RSS</a>";
+    }
 
 } else {
-
-    // DISPLAY ONE PIECE OF NEWS
 
     $news = new news(strip($_GET["id"]), "stringid");
 
     if ($news->isReal()) {
 
-        $news->display("main");
+        $new = $news->returnArray();
+
+        echo $twig->render("news-box.html", array("new" => $new, "main" => 0));
+
+        if ($new["comments"] == 1) {
+
+            $selectThreadId = $con->prepare("SELECT `forumthreads`.`id` FROM `forumthreads` WHERE `forumthreads`.`newsid` = :id");
+            $selectThreadId->bindValue("id", $news->getId(), PDO::PARAM_INT);
+            $selectThreadId->execute();
+
+            $threadData = $selectThreadId->fetch();
+
+            $tid = $threadData["id"];
+
+            require_once "inc/forums.php";
+
+        }
 
     } else {
 
