ecb241ef4455e993cf7d708286cdc06faa0f9a22
bognar marton <bognarmarci@gmail.com>
2014-02-08 23:43:40 +0100

    Fix a typo

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ function filterInput(elem) {
 
     if (elem.value.match(/\W/)) error += "Your username can contain English letters, numbers, and underscores only. ";
     if (!elem.value.match(/.{2}/)) error += "Your username must be at least 2 characters long. ";
-    if (elem.value.match(/.{11}/)) error += "Your username must be less, than 10 characters long. ";
+    if (elem.value.match(/.{11}/)) error += "Your username must be less than 10 characters long. ";
     var request = new XMLHttpRequest
 
     request.open('GET', '/inc/checkuser.php?name=' + elem.value, true)
