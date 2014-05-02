736d07f653edce11a7b2d06efcd6828f73db6cd6
bognar marton <bognarmarci@gmail.com>
2014-05-02 16:39:11 +0200

    Make returns user friendly

--- srv/updater.php
+++ srv/updater.php
@@ -3,5 +3,11 @@
 exec($config["python"]["github"], $output, $return);
 if ($return) error_log($return);
 
+echo "gaithub: ";
+print_r($return);
+
 exec($config["python"]["twitch"], $output, $return);
 if ($return) error_log($return);
+
+echo "twtich: ";
+print_r($return);
