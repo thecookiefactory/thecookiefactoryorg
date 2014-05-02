d5091cd8de765c28ca0e0fcdfe8eef2f26ea328b
bognar marton <bognarmarci@gmail.com>
2014-05-02 17:41:02 +0200

    Make Python scripts work on Heroku

--- srv/sql.py
+++ srv/sql.py
@@ -1,6 +1,24 @@
-import mysql.connector
-from config import *
+import os
+try:
+    from urlparse import urlsplit
+except ImportError:
+    from urllib.parse import urlsplit
 
+import mysql.connector
+try:
+    from config import *
+except ImportError:
+    constring = urlsplit(os.getenv('CLEARDB_DATABASE_URL'))
+    dbconfig = {
+        'user': constring.username,
+        'password': constring.password,
+        'host': constring.hostname,
+        'database': c.path[1:],
+    }
+
+    docroot = '/app'
+
+print('Initializing SQL connection.')
 
 class SQLConnection:
     def __init__(self):
