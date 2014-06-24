4fc89311b395158c92b1f66c5da3aa036904d7e5
bognar marton <bognarmarci@gmail.com>
2014-06-24 21:25:04 +0200

    Revert "Update updater.php"
    This reverts commit 377d21451f7a2baee51c364dcbde648cab70a6db.

--- srv/updater.php
+++ srv/updater.php
@@ -5,10 +5,10 @@ $r_c = 0;
 require_once "../inc/functions.php";
 
 if (!in_array($_SERVER['REMOTE_ADDR'], $config["updater_ip_whitelist"])) {
-    fwrite(STDOUT, "255");
+    error_log("255");
     die("255");
 }
 
 exec($config["python"]["updater"], $output, $return);
-fwrite(STDOUT, $output);
-fwrite(STDOUT, $return);
+error_log($output);
+error_log($return);
