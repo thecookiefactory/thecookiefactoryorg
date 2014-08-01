cae88337ff501e57771ac55788ec720554cca871
bognar marton <bognarmarci@gmail.com>
2014-08-01 04:32:22 +0200

    Templatify 404 errors

--- inc/404.php
+++ inc/404.php
@@ -8,11 +8,6 @@ $msgs = array("Houston, we have a four-oh-four.",
               "You get a 404, you get a 404, everybody gets a 404!",
               "A 404 a day keeps the content away.",
               "Do you believe in &#9835;four after four&#9835;? (With a zero inbetween.)",
-              "A 403 isn't cool. You know what's cool? A 404."); ?>
+              "A 403 isn't cool. You know what's cool? A 404.");
 
-<div class='notfound-box'></div>
-<div class='notfound-quote'>
-    <span class='notfound-quote-text'>&bdquo;<?php echo $msgs[array_rand($msgs)]; ?>&ldquo;</span>
-    <br>
-    <span class='notfound-quote-data'>&mdash;thecookiefactory, <?php $date = getdate(); echo($date["month"] . " " . $date["mday"] . ", " . $date["year"]); ?></span>
-</div>
+echo $twig->render("404.html", array("index_var" => $index_var, "message" => $msgs[array_rand($msgs)], "date" => getdate()));
