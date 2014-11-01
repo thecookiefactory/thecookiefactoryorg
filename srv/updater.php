6e7c78cead3844a7e66fa4b8fe9424f96440b5ad
marton bognar <bognarmarci@gmail.com>
2014-11-02 00:45:00 +0100

    Let's see what's wrong here

--- srv/updater.php
+++ srv/updater.php
@@ -3,12 +3,15 @@
 $r_c = 0;
 
 require_once "../inc/functions.php";
-
+/*
 if (!in_array($_SERVER['REMOTE_ADDR'], $config["updater_ip_whitelist"])) {
     error_log("255");
     die("255");
 }
-
+*/
 exec($config["python"]["updater"], $output, $return);
 error_log($output);
 error_log($return);
+
+echo 'output: <pre>'; print_r($output); echo '</pre>';
+echo 'return: <pre>'; print_r($return); echo '</pre>';
